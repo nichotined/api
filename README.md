f05f428fa1c0477806de9fcf1abc5084411f3a6d
Nicholas Frederich <nicholas.frederich.lagaunne@gmail.com>
2019-01-30 10:36:28 +0530

    Initialize commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# API
+
+This python package for API testing.
+Uses requests package
+https://github.com/requests/requests/
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="api-nichotined",
+    version="0.5.0",
+    author="Nicholas Frederick",
+    author_email="nicholas.frederich.lagaunne@gmail.com",
+    description="API package for testing purpose",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nichotined/api",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
