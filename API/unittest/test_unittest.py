a09e6c4dca675b609128884bdee87cc9326e5a96
Nicholas Frederich <nicholas.frederich.lagaunne@gmail.com>
2019-01-30 10:44:07 +0530

    refactor the unittest

--- API/unittest/test_unittest.py
+++ API/unittest/test_unittest.py
@@ -7,14 +7,8 @@ class TestClass:
     def test_get(self):
         api = API()
         get = api.get
-        get.url = "http://integration-customer-owner-internal.golabs.io/internal/customers/280523"
-        get.header = {
-            "client-id": "go-shop-integration",
-            "pass-key": "o3B3AgnyjD8zmlorYdrtxKRHFxxTwwZGQpQIAnA+ue0=",
-            "Content-Type": "application/json",
-            "Cache-Control": "no-cache",
-            "Postman-Token": "4a8ea09e-cd6d-57ab-04b6-047c3c9323e2"
-        }
+        get.url = "https://google.com"
+        get.header = {}
         get.body = {}
         get.params = {}
         get.timeout = 1
