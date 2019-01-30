7acd711e8bbc91de903f2151917f8fb97a6cd8aa
Nicholas Frederich <nicholas.frederich.lagaunne@gmail.com>
2019-01-30 10:49:24 +0530

    Add depedencies

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/nichotined/api",
     packages=setuptools.find_packages(),
+    install_requires=[
+          'requests',
+          'pytest',
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
