daa6a002ceccf30326ce8f6419de82f7f8a4e54a
Nicholas Frederich <nicholas.frederich.lagaunne@gmail.com>
2019-02-11 17:35:18 +0700

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="api-nichotined",
-    version="0.8.1",
+    version="0.8.2",
     author="Nicholas Frederick",
     author_email="nicholas.frederich.lagaunne@gmail.com",
     description="API package for testing purpose",
