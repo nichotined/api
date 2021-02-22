3c4f73ee4b8b4d5fd9b83e7de32ab93de0557f4f
nicholas frederick <nicholas.frederich.lagaunne@gmail.com>
2021-02-22 12:31:41 +0700

    Add mitmproxy mocker

---
+++ API/modules/mocker/mocker.py
@@ -0,0 +1,35 @@
+from mitmproxy import http
+
+
+class MockResponse:
+    def __init__(self, url: str, status_code: int, body: str, content_type: str):
+        self.url = url
+        self.status_code = status_code
+        self.body = body
+        self.content_type = {"Content-Type": content_type}
+
+
+class Events:
+    def __init__(self, mock_data: MockResponse):
+        self.mock_data = mock_data
+
+    def request(self, flow: http.HTTPRequest) -> None:
+        if self.mock_data.url in flow.request.url:
+            flow.response = http.HTTPResponse.make(
+                status_code=self.mock_data.status_code,
+                content=self.mock_data.body,
+                headers=self.mock_data.content_type
+            )
+
+
+j = """
+{
+"key":"value"
+}
+"""
+
+mr = MockResponse("https://google.com", 200, j, "application/json")
+
+addons = [
+    Events(mr)
+]
